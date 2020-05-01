330287a75f7b69a5b2de461ae07d517a93b42ca5
Alefe Mafra <emanoelmafra@gmail.com>
2020-05-01 20:39:08 -0300

    tamanhos mobile

---
+++ frontend/src/styles/device.js
@@ -0,0 +1,10 @@
+const device = {
+  mobileS: `(min-width: '320px')`,
+  mobileM: `(min-width: 375px)`,
+  mobileL: `(min-width: 425px)`,
+  tablet: `(min-width: 768px)`,
+  laptop: `(min-width: 1024px)`,
+  laptopL: `(min-width: 1301px)`,
+};
+
+export default device;
