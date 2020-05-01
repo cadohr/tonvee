e53ee686b273a09738411256e21fca31af9393bb
Ricardo Jacinto <cadohr@gmail.com>
2020-05-01 17:50:22 -0300

    add auth layout

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-// import AuthLayout from '~/pages/_layouts/auth';
+import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
 import { store } from '~/store';
@@ -12,7 +12,9 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const { signed } = store.getState().auth;
+  let { signed } = store.getState().auth;
+  // TODO: remove this line
+  signed = true; // alterar para false para mexer no login e true para mexer no resto
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -22,15 +24,15 @@ export default function RouteWrapper({
     return <Redirect to="/lobby" />;
   }
 
-  // const Layout = signed ? DefaultLayout : AuthLayout;
+  const Layout = signed ? DefaultLayout : AuthLayout;
 
   return (
     <Route
       {...rest}
       render={(props) => (
-        <DefaultLayout>
+        <Layout>
           <Component {...props} />
-        </DefaultLayout>
+        </Layout>
       )}
     />
   );
