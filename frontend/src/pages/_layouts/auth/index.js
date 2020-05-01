ab79ef079ccaba8c73374d608d793dcb56a34832
Ricardo Jacinto <cadohr@gmail.com>
2020-05-01 17:57:22 -0300

    fix auth layout

--- frontend/src/pages/_layouts/auth/index.js
+++ frontend/src/pages/_layouts/auth/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Wrapper } from './styles';
+
 export default function AuthLayout({ children }) {
   return <Wrapper>{children}</Wrapper>;
 }
