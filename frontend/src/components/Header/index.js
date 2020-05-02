1e9eb6ac7fcdffaa783052ae3b4e086579936133
Alefe Mafra <emanoelmafra@gmail.com>
2020-05-01 22:49:12 -0300

    Header Desktop Finalizado

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import user from '../../assets/user.png';
+import logout from '../../assets/logout.svg';
+import { Container, Logo, ShortCuts, UserProfiler, Logout } from './styles';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <Container>
+        <Logo>
+          <img src={logo} alt="Tonee" />
+        </Logo>
+        <ShortCuts>
+          <UserProfiler>
+            <Link to="/profile">
+              <img src={user} alt="User" />
+            </Link>
+          </UserProfiler>
+          <Logout>
+            <img src={logout} alt="Sair" />
+          </Logout>
+        </ShortCuts>
+      </Container>
+    </>
+  );
 }
---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #0aa1a7;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  margin: 10px 45px;
+`;
+
+export const ShortCuts = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 30px;
+`;
+
+export const UserProfiler = styled.div`
+  margin: 0 15px;
+
+  img {
+    max-width: 45px;
+    max-height: 45px;
+  }
+`;
+
+export const Logout = styled.div`
+  margin: 0 15px;
+
+  img {
+    max-width: 39px;
+    max-height: 39px;
+  }
+`;
