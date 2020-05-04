c5bf37e7422a9266ccd24fd16e67db84380eb003
Ricardo Jacinto <cadohr@gmail.com>
2020-05-04 14:33:21 -0300

    add env

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketIoClient from 'socket.io-client';
 
-const url = process.env.REACT_APP_APP_URL || 'http://localhost:3333';
+const url = process.env.REACT_APP_API_URL || 'http://localhost:3333';
 
 const socket = socketIoClient(url);
 
